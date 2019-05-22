2c2d04d33e4fe5fb5e8f5db3d9596f4ebbf830f3
hannah <hannahjihyelim@gmail.com>
2019-05-22 17:32:22 +0900

    update MemberDAO and Controller by jihye. 05/22-17:32

--- src/semi/test/dao/MemberDAO.java
+++ src/semi/test/dao/MemberDAO.java
@@ -20,17 +20,18 @@ public class MemberDAO {
 	}
 	
 	private PreparedStatement pstatForInsertMember(Connection con, MemberDTO dto) throws Exception {
-		String sql = "insert into members values (?,?,?,?,?,?,?,default,?,?)";
+		String sql = "insert into members values (?,?,?,?,?,?,?,?,default,?,?)";
 		PreparedStatement pstat = con.prepareStatement(sql);
 		pstat.setString(1, dto.getEmail());
 		pstat.setString(2, dto.getPw());
 		pstat.setString(3, dto.getName());
-		pstat.setString(4, dto.getPhone());
-		pstat.setString(5, dto.getZipCode());
-		pstat.setString(6, dto.getAddress1());
-		pstat.setString(7, dto.getAddress2());
-		pstat.setString(8, dto.getIpAddress());
-		pstat.setString(9, dto.getAdmin());
+		pstat.setString(4, dto.getNickname());
+		pstat.setString(5, dto.getPhone());
+		pstat.setString(6, dto.getZipCode());
+		pstat.setString(7, dto.getAddress1());
+		pstat.setString(8, dto.getAddress2());
+		pstat.setString(9, dto.getIpAddress());
+		pstat.setString(10, dto.getAdmin());
 		return pstat;
 	}
 	
@@ -58,13 +59,14 @@ public class MemberDAO {
 				dto.setEmail(rs.getString(1));
 				dto.setPw(rs.getString(2));
 				dto.setName(rs.getString(3));
-				dto.setPhone(rs.getString(4));
-				dto.setZipCode(rs.getString(5));
-				dto.setAddress1(rs.getString(6));
-				dto.setAddress2(rs.getString(7));
-				dto.setJoinDate(rs.getTimestamp(8));
-				dto.setIpAddress(rs.getString(9));
-				dto.setAdmin(rs.getString(10));
+				dto.setNickname(rs.getString(4));
+				dto.setPhone(rs.getString(5));
+				dto.setZipCode(rs.getString(6));
+				dto.setAddress1(rs.getString(7));
+				dto.setAddress2(rs.getString(8));
+				dto.setJoinDate(rs.getTimestamp(9));
+				dto.setIpAddress(rs.getString(10));
+				dto.setAdmin(rs.getString(11));
 				result.add(dto);
 			}
 			return result;
