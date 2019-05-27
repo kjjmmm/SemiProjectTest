24273b41364283f4d24e83bcad696e04e1d168e0
JeonHaeYong <50863296+JeonHaeYong@users.noreply.github.com>
2019-05-27 19:41:41 +0900

    Update MemberDAO.java

--- src/kh/semi/dao/MemberDAO.java
+++ src/kh/semi/dao/MemberDAO.java
@@ -200,8 +200,8 @@ class MyAuthentication extends Authenticator {
 
 	public MyAuthentication(){
 
-		String id = "junhaeyong95@gmail.com";       // 구글 ID
-		String pw = "wjsgodyd95!!";          // 구글 비밀번호
+		String id = "";       // 구글 ID
+		String pw = "";          // 구글 비밀번호
 
 		// ID와 비밀번호를 입력한다.
 		pa = new PasswordAuthentication(id, pw);
